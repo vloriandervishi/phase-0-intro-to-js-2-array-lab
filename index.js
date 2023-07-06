f744e2ddb76d5925d5be59d585c6d111d8f3d627
vlorian <vlorian.dervishi@gmail.com>
2023-07-06 17:49:53 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+function appendCat(name) {
+    const fursBall = [...cats, name]
+    return fursBall;
+}
+function prependCat(name) {
+    const fursball = [name, ...cats]
+    return fursball;
+}
+
+function removeLastCat() {
+    const fursBall = [...cats];
+    fursBall.pop();
+    return fursBall
+}
+function removeFirstCat() {
+    const fursBall = [...cats];
+    fursBall.shift();
+    return fursBall;
+}
